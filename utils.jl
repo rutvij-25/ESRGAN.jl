01666d75c18d9c3aa53c1f1adf5f085e8b470d98
Rutvij Wamanse <wamanserutvij25@gmail.com>
2021-07-24 00:38:04 +0530

    dataloader

--- utils.jl
+++ utils.jl
@@ -13,13 +13,11 @@ function get_batch(path,idxs)
     img_HR = []
     img_LR = []
     for img in imgs
-        push!(img_HR,load_image(img,200))
-        push!(img_LR,load_image(img,50))
+        push!(img_HR,permutedims(load_image(img,200),(2,3,1)))
+        push!(img_LR,permutedims(load_image(img,50),(2,3,1)))
     end
     img_HR = cat(img_HR...,dims=4)
-    img_HR = permutedims(img_HR,(2,3,1,4))
     img_LR = cat(img_LR...,dims=4)
-    img_LR = permutedims(img_LR,(2,3,1,4))
     return img_HR,img_LR
 end
 
